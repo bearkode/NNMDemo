fd82e9d4a65199fd267f40577d80df7ff11818c4
Chang-Gi, Kim <cgkim@mac.com>
2014-01-02 18:09:57 +0900

    change project settings, coding conventions

--- NNMDemo/BCAppDelegate.h
+++ NNMDemo/BCAppDelegate.h
@@ -1,15 +1,19 @@
-//
-//  BCAppDelegate.h
-//  NNMDemo
-//
-//  Created by cgkim on 2014. 1. 2..
-//  Copyright (c) 2014년 bearkode. All rights reserved.
-//
+/*
+ *  BCAppDelegate.h
+ *  NNMDemo
+ *
+ *  Created by bearkode on 2014. 1. 2..
+ *  Copyright (c) 2014 bearkode. All rights reserved.
+ *
+ */
 
 #import <UIKit/UIKit.h>
 
+
 @interface BCAppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic, retain) UIWindow *window;
+
 
 @end
--- NNMDemo/BCAppDelegate.m
+++ NNMDemo/BCAppDelegate.m
@@ -1,49 +1,65 @@
-//
-//  BCAppDelegate.m
-//  NNMDemo
-//
-//  Created by cgkim on 2014. 1. 2..
-//  Copyright (c) 2014년 bearkode. All rights reserved.
-//
+/*
+ *  BCAppDelegate.m
+ *  NNMDemo
+ *
+ *  Created by bearkode on 2014. 1. 2..
+ *  Copyright (c) 2014 bearkode. All rights reserved.
+ *
+ */
 
 #import "BCAppDelegate.h"
 
-@implementation BCAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+@implementation BCAppDelegate
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+    UIWindow *mWindow;
+}
+
+
+@synthesize window = mWindow;
+
+
+#pragma mark -
+
+
+- (BOOL)application:(UIApplication *)aApplication didFinishLaunchingWithOptions:(NSDictionary *)aLaunchOptions
+{
+    mWindow = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [mWindow setBackgroundColor:[UIColor whiteColor]];
+    [mWindow makeKeyAndVisible];
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
+
+- (void)applicationWillResignActive:(UIApplication *)aApplication
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
+
+- (void)applicationDidEnterBackground:(UIApplication *)aApplication
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
+
+- (void)applicationWillEnterForeground:(UIApplication *)aApplication
 {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
+
+- (void)applicationDidBecomeActive:(UIApplication *)aApplication
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
+
+- (void)applicationWillTerminate:(UIApplication *)aApplication
 {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+
 }
+
 
 @end
